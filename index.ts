cf87a51eb4a2de51eaddea8997bbb197e0877c92
aidakhome <hello@aidak.xyz>
2023-02-13 17:05:19 +0100

    🖋  new era

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /**
  * 
- * Copyright 2022 Aidak.
+ * Copyright 2022-2023 wiletki
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
